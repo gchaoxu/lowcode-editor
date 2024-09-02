57943992389cef03e982c871f10605ea2874a6d1
gchaoxu <xgc0829@163.com>
2024-09-02 15:15:30 +0800

    核心数据结构和全局 store

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,3 +1,24 @@
+import { useEffect } from 'react';
+import { useComponetsStore } from '../stores/components';
+
 export function EditArea() {
-  return <div>EditArea</div>;
+  const { components, addComponent } = useComponetsStore();
+
+  useEffect(() => {
+    addComponent(
+      {
+        id: 222,
+        name: 'Container',
+        props: {},
+        children: [],
+      },
+      1
+    );
+  }, []);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(components, null, 2)}</pre>
+    </div>
+  );
 }
