271279fe39a867debaef3280ce39a390fa231c48
gchaoxu <xgc0829@163.com>
2024-09-10 18:36:13 +0800

    预览 大纲

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -6,25 +6,33 @@ import { EditArea } from './components/EditArea/EditArea';
 // 属性编辑区
 import { Setting } from './components/Setting';
 // 物料区
-import { Material } from './components/Material';
+import { MaterialWrapper } from './components/MaterialWrapper';
+// 预览
+import { Preview } from './components/Prview';
+import { useComponetsStore } from './stores/components';
 
 export default function ReactPlayground() {
+  const { mode } = useComponetsStore();
   return (
     <div className="h-[100vh] flex flex-col">
       <div className="h-[60px] flex items-center border-b-[1px] border-[#000]">
         <Header />
       </div>
-      <Allotment>
-        <Allotment.Pane preferredSize={240} maxSize={300} minSize={200}>
-          <Material />
-        </Allotment.Pane>
-        <Allotment.Pane>
-          <EditArea />
-        </Allotment.Pane>
-        <Allotment.Pane preferredSize={300} maxSize={500} minSize={300}>
-          <Setting />
-        </Allotment.Pane>
-      </Allotment>
+      {mode === 'edit' ? (
+        <Allotment>
+          <Allotment.Pane preferredSize={240} maxSize={300} minSize={200}>
+            <MaterialWrapper />
+          </Allotment.Pane>
+          <Allotment.Pane>
+            <EditArea />
+          </Allotment.Pane>
+          <Allotment.Pane preferredSize={300} maxSize={500} minSize={300}>
+            <Setting />
+          </Allotment.Pane>
+        </Allotment>
+      ) : (
+        <Preview />
+      )}
     </div>
   );
 }
